8f38cb7a2b938f9e2725df18e1278d99ff3e083f
Merle <merleself@gmail.com>
2022-05-07 16:37:10 -0400

    plant model finished

---
+++ server/db/models/plants.js
@@ -0,0 +1,44 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Plant = db.define('plant', {
+  plantName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  potSize: {
+    type: Sequelize.ENUM,
+    values: ['2"', '4"', '6"', '8"', '10"'],
+    allowNull: false
+  },
+  care: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    defaultValue: 15.99
+  },
+  qty: {
+    type: Sequelize.DECIMAL,
+    defaultValue: 1,
+    allowNull: false,
+    validate: {
+      max: 100,
+      min: 0
+    }
+  },
+  picture: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    defaultValue:
+      'https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/63919/potted-plant-clipart-xl.png'
+  }
+})
+
+module.exports = Plant
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -6,26 +6,29 @@ const User = db.define('user', {
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
   },
   password: {
     type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('password')
     }
   },
   salt: {
     type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('salt')
     }
   },
   googleId: {
     type: Sequelize.STRING
+  },
+  isAdmin: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
   }
 })
 
