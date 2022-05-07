983d197bde47a26a5a082907adafa6e3458c5196
Merle <merleself@gmail.com>
2022-05-07 16:42:50 -0400

    cartDetails model done

--- server/db/models/cartDetails.js
+++ server/db/models/cartDetails.js
@@ -1,2 +1,13 @@
 const Sequelize = require('sequelize')
 const db = require('../db')
+
+const CartDetails = db.define('cartDetail', {
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      min: 0
+    }
+  }
+})
+
+module.exports = CartDetails
